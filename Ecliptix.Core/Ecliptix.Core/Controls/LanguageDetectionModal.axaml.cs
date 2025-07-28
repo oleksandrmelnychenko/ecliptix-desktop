505bef13d1471580276bba01856db89fceb41bb6
Vitaliy <vitalik.koliesnikov@gmail.com>
2025-07-28 12:13:52 +0300

    feat: add LanguageDetectionModal and update UI elements for improved aesthetics

---
+++ Ecliptix.Core/Ecliptix.Core/Controls/LanguageDetectionModal.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ecliptix.Core.Controls;
+
+public partial class LanguageDetectionModal : UserControl
+{
+    public LanguageDetectionModal()
+    {
+        InitializeComponent();
+    }
+}
