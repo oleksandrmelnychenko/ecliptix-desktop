54a92d9e6e735e3a0dc0dc934d8de26586f75304
Olek <oleksandr.melnychenko@live.com>
2025-07-30 18:45:23 +0300

    feat: add country culture mismatch check and improve connectivity handling in MembershipHostWindowModel

--- Ecliptix.Core/Ecliptix.Core/ViewModels/Memberships/MembershipHostWindowModel.cs
+++ Ecliptix.Core/Ecliptix.Core/ViewModels/Memberships/MembershipHostWindowModel.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 using Ecliptix.Core.AppEvents.BottomSheet;
 using Ecliptix.Core.AppEvents.System;
@@ -15,22 +18,26 @@ using Ecliptix.Core.ViewModels.Memberships.SignUp;
 using Ecliptix.Protobuf.AppDevice;
 using Ecliptix.Utilities;
 using ReactiveUI;
+using Serilog;
 using Unit = System.Reactive.Unit;
 
 namespace Ecliptix.Core.ViewModels.Memberships;
 
 public class MembershipHostWindowModel : ViewModelBase, IScreen
 {
-    private readonly IBottomSheetEvents _bottomSheetEvents;
-    public RoutingState Router { get; } = new();
-
-    public ReactiveCommand<MembershipViewType, IRoutableViewModel> Navigate { get; }
-
-    private readonly IDisposable _connectivitySubscription;
     private bool _isConnected = true;
     private bool _canNavigateBack;
+    private readonly IBottomSheetEvents _bottomSheetEvents;
+    private readonly ISecureStorageProvider _secureStorageProvider;
+    private readonly IDisposable _connectivitySubscription;
 
-    private ApplicationInstanceSettings _applicationInstanceSettings;
+    private static readonly IReadOnlyDictionary<string, string> SupportedCountries = new Dictionary<string, string>
+    {
+        { "UA", "uk-UA" },
+        { "US", "en-US" },
+    }.AsReadOnly();
+
+    public RoutingState Router { get; } = new();
 
     public bool IsConnected
     {
@@ -44,11 +51,17 @@ public class MembershipHostWindowModel : ViewModelBase, IScreen
         private set => this.RaiseAndSetIfChanged(ref _canNavigateBack, value);
     }
 
+    public LanguageSwitcherViewModel LanguageSwitcher { get; }
+
+    public ReactiveCommand<MembershipViewType, IRoutableViewModel> Navigate { get; }
+
     public ReactiveCommand<Unit, Unit> OpenPrivacyPolicyCommand { get; }
+
     public ReactiveCommand<Unit, Unit> OpenTermsOfServiceCommand { get; }
+
     public ReactiveCommand<Unit, Unit> OpenSupportCommand { get; }
 
-    public LanguageSwitcherViewModel LanguageSwitcher { get; }
+    public ReactiveCommand<Unit, Unit> CheckCountryCultureMismatchCommand { get; }
 
     public MembershipHostWindowModel(
         IBottomSheetEvents bottomSheetEvents,
@@ -57,35 +70,14 @@ public class MembershipHostWindowModel : ViewModelBase, IScreen
         ILocalizationService localizationService,
         InternetConnectivityObserver connectivityObserver,
         ISecureStorageProvider secureStorageProvider,
-        IAuthenticationService authenticationService
-    ) : base(systemEvents, networkProvider, localizationService)
+        IAuthenticationService authenticationService)
+        : base(systemEvents, networkProvider, localizationService)
     {
-        _bottomSheetEvents = bottomSheetEvents;
-        _connectivitySubscription = connectivityObserver.Subscribe(async status => { IsConnected = status; });
+        _bottomSheetEvents = bottomSheetEvents ?? throw new ArgumentNullException(nameof(bottomSheetEvents));
+        _secureStorageProvider = secureStorageProvider;
 
         LanguageSwitcher = new LanguageSwitcherViewModel(localizationService, secureStorageProvider);
-
-        _ = Task.Run(async () =>
-        {
-            Result<ApplicationInstanceSettings, InternalServiceApiFailure> appSettings =
-                await secureStorageProvider.GetApplicationInstanceSettingsAsync();
-
-            if (appSettings.IsOk)
-            {
-                ApplicationInstanceSettings applicationInstanceSettings = appSettings.Unwrap();
-                string country = applicationInstanceSettings.Country;
-                if (!string.IsNullOrEmpty(country))
-                {
-                    string expectedCulture = country switch
-                    {
-                        "UA" => "uk-UA",
-                        _ => "en-US" 
-                    };
-                    
-                    
-                }
-            }
-        });
+        _connectivitySubscription = connectivityObserver.Subscribe(status => { IsConnected = status; });
 
         Navigate = ReactiveCommand.CreateFromObservable<MembershipViewType, IRoutableViewModel>(viewType =>
             Router.Navigate.Execute(
@@ -93,26 +85,59 @@ public class MembershipHostWindowModel : ViewModelBase, IScreen
                     authenticationService)
             ));
 
-        Navigate.Execute(MembershipViewType.MembershipWelcome).Subscribe();
+        CheckCountryCultureMismatchCommand = ReactiveCommand.CreateFromTask(async () =>
+        {
+            await CheckCountryCultureMismatchAsync();
+            return Unit.Default;
+        });
+
+        OpenPrivacyPolicyCommand = ReactiveCommand.Create(() => OpenUrl("https://ecliptix.com/privacy"));
+        OpenTermsOfServiceCommand = ReactiveCommand.Create(() => OpenUrl("https://ecliptix.com/terms"));
+        OpenSupportCommand = ReactiveCommand.Create(() => OpenUrl("https://ecliptix.com/support"));
 
         this.WhenAnyObservable(x => x.Router.NavigateBack.CanExecute)
             .Subscribe(canExecute => { CanNavigateBack = canExecute; });
 
-        OpenPrivacyPolicyCommand = ReactiveCommand.Create(() => { OpenUrl("https://ecliptix.com/privacy"); });
+        this.WhenActivated(disposables =>
+        {
+            Observable.Timer(TimeSpan.FromSeconds(2), RxApp.MainThreadScheduler)
+                .SelectMany(_ => CheckCountryCultureMismatchCommand.Execute())
+                .Subscribe(_ => { })
+                .DisposeWith(disposables);
 
-        OpenTermsOfServiceCommand = ReactiveCommand.Create(() => { OpenUrl("https://ecliptix.com/terms"); });
-
-        OpenSupportCommand = ReactiveCommand.Create(() => { OpenUrl("https://ecliptix.com/support"); });
+            Navigate.Execute(MembershipViewType.MembershipWelcome)
+                .Subscribe()
+                .DisposeWith(disposables);
+        });
     }
 
-    private void ShowSimpleBottomSheet()
+    private async Task CheckCountryCultureMismatchAsync()
     {
-        _bottomSheetEvents.BottomSheetChangedState(
-            BottomSheetChangedEvent.New(BottomSheetComponentType.DetectedLocalization));
+        Result<ApplicationInstanceSettings, InternalServiceApiFailure> appSettings =
+            await _secureStorageProvider.GetApplicationInstanceSettingsAsync();
+
+        if (appSettings.IsOk)
+        {
+            ApplicationInstanceSettings applicationInstanceSettings = appSettings.Unwrap();
+            if (!string.IsNullOrEmpty(applicationInstanceSettings.Country))
+            {
+                string currentCulture = System.Globalization.CultureInfo.CurrentUICulture.Name;
+
+                string expectedCulture =
+                    SupportedCountries.GetValueOrDefault(applicationInstanceSettings.Country, "en-US");
+
+                if (!string.Equals(currentCulture, expectedCulture, StringComparison.OrdinalIgnoreCase))
+                {
+                    _bottomSheetEvents.BottomSheetChangedState(
+                        BottomSheetChangedEvent.New(BottomSheetComponentType.DetectedLocalization));
+                }
+            }
+        }
     }
 
     private static void OpenUrl(string url)
     {
+        Log.Information($"Opening URL: {url}");
     }
 
     private IRoutableViewModel CreateViewModelForView(
@@ -120,8 +145,7 @@ public class MembershipHostWindowModel : ViewModelBase, IScreen
         MembershipViewType viewType,
         NetworkProvider networkProvider,
         ILocalizationService localizationService,
-        IAuthenticationService authenticationService
-    )
+        IAuthenticationService authenticationService)
     {
         return viewType switch
         {
@@ -130,16 +154,24 @@ public class MembershipHostWindowModel : ViewModelBase, IScreen
             MembershipViewType.MembershipWelcome => new WelcomeViewModel(this, systemEvents, localizationService,
                 networkProvider),
             MembershipViewType.PhoneVerification => new MobileVerificationViewModel(systemEvents, networkProvider,
-                localizationService,
-                this),
-            MembershipViewType.VerificationCodeEntry => new VerificationCodeEntryViewModel(systemEvents,
-                networkProvider,
                 localizationService, this),
+            MembershipViewType.VerificationCodeEntry => new VerificationCodeEntryViewModel(systemEvents,
+                networkProvider, localizationService, this),
             MembershipViewType.ConfirmPassword => new PasswordConfirmationViewModel(systemEvents, networkProvider,
                 localizationService, this),
             MembershipViewType.PassPhase => new PassPhaseViewModel(systemEvents, localizationService, this,
                 networkProvider),
-            _ => throw new ArgumentOutOfRangeException(nameof(viewType)),
+            _ => throw new ArgumentOutOfRangeException(nameof(viewType))
         };
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            _connectivitySubscription.Dispose();
+        }
+
+        base.Dispose(disposing);
     }
 }
