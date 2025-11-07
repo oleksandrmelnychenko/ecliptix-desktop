7dc3b0d7036c9b6cfda2ecfd7d6f2cb24f3aa3ac
Vitaliy <vitalik.koliesnikov@gmail.com>
2025-11-07 15:18:23 +0200

    feat(TitleBar): update styles and bindings for non-Windows/macOS platforms
    - Adjusted `ContentControl` style to exclude Windows and macOS.
    - Removed default values for button disable properties in `TitleBarViewModel`.
    - Added data type binding for `WindowsTitleBarLayout`.

--- Ecliptix.Core/Ecliptix.Core/Views/Memberships/Components/TitleBarViewModel.cs
+++ Ecliptix.Core/Ecliptix.Core/Views/Memberships/Components/TitleBarViewModel.cs
@@ -6,8 +6,8 @@ namespace Ecliptix.Core.Views.Memberships.Components;
 public class TitleBarViewModel : ReactiveObject
 {
 
-    [Reactive] public bool DisableCloseButton { get; set; } = false;
-    [Reactive] public bool DisableMinimizeButton { get; set; } = false;
-    [Reactive] public bool DisableMaximizeButton { get; set; } = false;
+    [Reactive] public bool DisableCloseButton { get; set; }
+    [Reactive] public bool DisableMinimizeButton { get; set; }
+    [Reactive] public bool DisableMaximizeButton { get; set; }
 
 }
