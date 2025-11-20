1e7ebcd7a0de7f8c8e040a1c25969da0b250acbf
Vitaliy <vitalik.koliesnikov@gmail.com>
2025-11-20 17:35:55 +0200

    feat(MasterView, SettingsView): refactor layout and remove unused code
    * Cleaned up `MasterView.axaml` by removing unused namespaces.
    * Streamlined `ProfileView.axaml` by fixing indentation and removing unnecessary whitespace.
    * Refactored `SettingsView.axaml` to improve layout structure and readability.
    * Enhanced transition handling in `SettingsView.axaml` for better user experience.

--- Ecliptix.Core/Ecliptix.Core/Features/Main/Views/MasterView.axaml.cs
+++ Ecliptix.Core/Ecliptix.Core/Features/Main/Views/MasterView.axaml.cs
@@ -1,15 +1,5 @@
-using System;
-using System.Reactive.Disposables;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Input;
 using Avalonia.Markup.Xaml;
-using Ecliptix.Core.Core.Messaging;
-using Ecliptix.Core.Core.Messaging.Events;
-using Ecliptix.Core.Core.Messaging.Services;
-using Ecliptix.Core.Features.Main.ViewModels;
-using Serilog;
-using Splat;
 
 namespace Ecliptix.Core.Features.Main.Views;
 
