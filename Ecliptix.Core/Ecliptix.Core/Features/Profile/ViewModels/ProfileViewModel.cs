9c2b2b8609381bfa26de5ddddf847a3f3600613d
Vitaliy <vitalik.koliesnikov@gmail.com>
2025-11-21 12:35:50 +0200

    feat(Profile): adjust padding and update icon data in profile view
    * Reduced `Padding` from `12,6` to `10,6` for better spacing.
    * Updated `IconData` for various menu items to enhance visual consistency.

--- Ecliptix.Core/Ecliptix.Core/Features/Profile/ViewModels/ProfileViewModel.cs
+++ Ecliptix.Core/Ecliptix.Core/Features/Profile/ViewModels/ProfileViewModel.cs
@@ -47,7 +47,7 @@ public class ProfileViewModel : Core.MVVM.ViewModelBase, IActivatableViewModel
         PersonalLikesProfileViewModel likesVm = new();
         PersonalFavoritesViewModel favoritesVm = new();
 
-        MenuItems = new ObservableCollection<ProfileMenuItem>
+       MenuItems = new ObservableCollection<ProfileMenuItem>
         {
             new()
             {
@@ -59,25 +59,25 @@ public class ProfileViewModel : Core.MVVM.ViewModelBase, IActivatableViewModel
             new()
             {
                 Title = "Posts",
-                IconData = "M3 3v18h18V3H3zm16 16H5V5h14v14zM11 13h2v2h-2v-2zm-4 0h2v2H7v-2zm8 0h2v2h-2v-2zm-8-4h2v2H7V9zm4 0h2v2h-2V9zm4 0h2v2h-2V9z",
+                IconData = "M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM7 19h14v-2H7v2zm0-6h14v-2H7v2zm0-8v2h14V5H7z",
                 ViewModel = postsVm,
             },
             new()
             {
                 Title = "Media",
-                IconData = "M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z",
+                IconData = "M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-4.86 8.86l-3 3.87L9 13.14 6 17h12l-3.86-5.14z",
                 ViewModel = imagesVm
             },
             new()
             {
                 Title = "Likes",
-                IconData = "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z",
+                IconData = "M16.5 3c-1.74 0-3.41.81-4.5 2.09C10.91 3.81 9.24 3 7.5 3 4.42 3 2 5.42 2 8.5c0 3.78 3.4 6.86 8.55 11.54L12 21.35l1.45-1.32C18.6 15.36 22 12.28 22 8.5 22 5.42 19.58 3 16.5 3zm-4.4 15.55l-.1.1-.1-.1C7.14 14.24 4 11.39 4 8.5 4 6.5 5.5 5 7.5 5c1.54 0 3.04.99 3.57 2.36h1.87C13.46 5.99 14.96 5 16.5 5c2 0 3.5 1.5 3.5 3.5 0 2.89-3.14 5.74-7.9 10.05z",
                 ViewModel = likesVm
             },
             new()
             {
                 Title = "Favorites",
-                IconData = "M17 3H7c-1.1 0-1.99.9-1.99 2L5 21l7-3 7 3V5c0-1.1-.9-2-2-2z",
+                IconData = "M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z",
                 ViewModel = favoritesVm
             }
         };
