c105827a791bd240d6985b98054c520370d12395
Vitaliy <vitalik.koliesnikov@gmail.com>
2025-12-11 14:26:03 +0200

    feat ( ui ) added aura logo animatino

---
+++ Ecliptix.Core/Ecliptix.Core/Controls/Illustrations/AuraProtectionSystem.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Ecliptix.Core.Controls.Illustrations;
+
+public partial class AuraProtectionSystem : UserControl
+{
+    public AuraProtectionSystem()
+    {
+        InitializeComponent();
+    }
+}
+
