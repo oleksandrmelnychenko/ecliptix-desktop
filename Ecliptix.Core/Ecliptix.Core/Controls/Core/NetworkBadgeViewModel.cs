b4e67c0cf11d953f31bd1daca9fdc359bca0b340
Vitaliy <vitalik.koliesnikov@gmail.com>
2025-12-17 11:40:05 +0200

    feat (badge) fixed statsu badge - fixed language change logic in network badge - fixed icons logic, made the same icons as in notification - added new lucide icons - fixed dimenstion in ConnectivityNotificationView.axaml

--- Ecliptix.Core/Ecliptix.Core/Controls/Core/NetworkBadgeViewModel.cs
+++ Ecliptix.Core/Ecliptix.Core/Controls/Core/NetworkBadgeViewModel.cs
@@ -12,22 +12,11 @@ using Splat;
 
 namespace Ecliptix.Core.Controls.Core;
 
-public enum NetworkState
-{
-    Disconnected,
-    Connected,
-    ServerError
-}
-
 public class NetworkBadgeViewModel : ReactiveObject, IDisposable
 {
     private readonly CompositeDisposable _disposables = new();
     private readonly IConnectivityService _connectivityService;
     private readonly ILocalizationService? _localizationService;
-
-    private const string IconOnline = "M5 12.55a11 11 0 0 1 14.08 0M1.42 9a16 16 0 0 1 21.16 0M8.53 16.11a6 6 0 0 1 6.95 0M12 20h.01";
-    private const string IconOffline = "M23.64 7c-.45-.34-4.93-4-11.64-4-1.5 0-2.89.19-4.15.48L18.18 13.8 23.64 7zm-6.6 8.22L3.27 1.44 2 2.72l2.05 2.06C1.91 5.17 1.5 5.48 1.5 5.48c-.5.39.06 1.07.57 1.07.21 0 .4-.09.52-.23 0 0 4.05-3.15 9.41-3.15 1.56 0 3 .26 4.31.7L18.7 6.3C17.06 5.86 15.09 5.5 13 5.5c-6.14 0-10.3 3.43-10.66 3.73l9.66 12.02c.3.37.86.37 1.16 0l2.76-3.44 3.4 3.41L20.71 20 17.04 15.22z";
-    private const string IconError = "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z";
 
     [ObservableAsProperty] public string Text { get; }
     [ObservableAsProperty] public string Icon { get; }
@@ -38,12 +27,11 @@ public class NetworkBadgeViewModel : ReactiveObject, IDisposable
     [ObservableAsProperty] public bool IsDisconnected { get; }
     [ObservableAsProperty] public bool IsServerError { get; }
 
-   public NetworkBadgeViewModel()
-    {
-        _connectivityService = Locator.Current.GetService<IConnectivityService>()
-            ?? throw new InvalidOperationException("IConnectivityService not found in Locator");
+   public NetworkBadgeViewModel(ILocalizationService localizationService, IConnectivityService connectivityService)
+   {
+       _localizationService = localizationService;
 
-        _localizationService = Locator.Current.GetService<ILocalizationService>();
+       _connectivityService = connectivityService;
 
         ConnectivityStatus initialInternetState = _connectivityService.LastKnownInternetStatus;
         ConnectivityStatus initialServerState = _connectivityService.LastKnownServerStatus;
@@ -91,33 +79,29 @@ public class NetworkBadgeViewModel : ReactiveObject, IDisposable
             .ToPropertyEx(this, x => x.IsServerError)
             .DisposeWith(_disposables);
 
+        IObservable<System.Reactive.Unit> languageChangedTrigger;
+
         if (_localizationService != null)
         {
-            Observable.CombineLatest(
-                    badgeState,
-                    _localizationService.WhenAnyValue(x => x.CurrentCultureName),
-                    (state, _) => GetTextForState(state))
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .ToPropertyEx(this, x => x.Text)
-                .DisposeWith(_disposables);
+            languageChangedTrigger = Observable.FromEvent(
+                    h => _localizationService.LanguageChanged += h,
+                    h => _localizationService.LanguageChanged -= h)
+                .Select(_ => System.Reactive.Unit.Default)
+                .StartWith(System.Reactive.Unit.Default);
         }
         else
         {
-            badgeState.Select(GetTextForState)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .ToPropertyEx(this, x => x.Text)
-                .DisposeWith(_disposables);
+            languageChangedTrigger = Observable.Return(System.Reactive.Unit.Default);
         }
 
-        badgeState.Select(GetHoverTextForState)
+        Observable.CombineLatest(
+                badgeState,
+                languageChangedTrigger,
+                (state, _) => GetTextForState(state))
             .ObserveOn(RxApp.MainThreadScheduler)
-            .ToPropertyEx(this, x => x.HoverText)
+            .ToPropertyEx(this, x => x.Text)
             .DisposeWith(_disposables);
 
-        badgeState.Select(GetIconForState)
-            .ObserveOn(RxApp.MainThreadScheduler)
-            .ToPropertyEx(this, x => x.Icon)
-            .DisposeWith(_disposables);
     }
 
     private NetworkBadgeState DetermineBadgeState(ConnectivityStatus internet, ConnectivityStatus server)
@@ -139,7 +123,6 @@ public class NetworkBadgeViewModel : ReactiveObject, IDisposable
             return NetworkBadgeState.Disconnected;
         }
 
-        // 4. Тільки якщо сервер чітко повернув помилку або DISCONNECTED при живому інтернеті
         return NetworkBadgeState.ServerError;
     }
 
@@ -149,7 +132,6 @@ public class NetworkBadgeViewModel : ReactiveObject, IDisposable
         {
             return state switch
             {
-                    //TODO localize
                 NetworkBadgeState.Connected => "Online",
                 NetworkBadgeState.ServerError => "Server Error",
                 _ => "Offline"
@@ -163,21 +145,6 @@ public class NetworkBadgeViewModel : ReactiveObject, IDisposable
             _ => _localizationService[LocalizationKeys.NetworkStatus.OFFLINE]
         };
     }
-
-    private string GetHoverTextForState(NetworkBadgeState state) => state switch
-    {
-        NetworkBadgeState.Connected => "You are connected to the interned",
-        NetworkBadgeState.ServerError => "No connection to the server, reconnecting",
-        _ => "You are not connected to the internet"
-    };
-
-    private string GetIconForState(NetworkBadgeState state) => state switch
-    {
-        NetworkBadgeState.Connected => IconOnline,
-        NetworkBadgeState.ServerError => IconError,
-        _ => IconOffline
-    };
-
     public void Dispose()
     {
         Log.Debug("[BadgeVM] Disposing NetworkBadgeViewModel");
