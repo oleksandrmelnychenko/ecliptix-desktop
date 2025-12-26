2588d314e929803e47b31c4bd5372e011d10b54b
Vitaliy <vitalik.koliesnikov@gmail.com>
2025-12-26 11:28:06 +0200

    fix(authentication) fixed login logic - fixed minimal and maximum dimenstion in OverlaySheetControl.axaml - fixed invokatin from invalid thread

--- Ecliptix.Core/Ecliptix.Core/Views/Core/MainWindow.cs
+++ Ecliptix.Core/Ecliptix.Core/Views/Core/MainWindow.cs
@@ -232,7 +232,14 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>, IDisposab
         {
             if (!_isDisposed)
             {
-                Close();
+                if (!Dispatcher.UIThread.CheckAccess())
+                {
+                    Dispatcher.UIThread.Post(() => Close());
+                }
+                else
+                {
+                    Close();
+                }
             }
         }
     }
