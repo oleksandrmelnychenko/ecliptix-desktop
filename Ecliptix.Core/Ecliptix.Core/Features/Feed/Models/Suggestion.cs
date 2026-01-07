9034e785bfa9edd7326fe750ff9c31b69bb1eecc
Volodymyr Ivanov <ivanov_va@hotmail.com>
2026-01-07 13:21:53 +0200

    The "following" indicator has been added.

--- Ecliptix.Core/Ecliptix.Core/Features/Feed/Models/Suggestion.cs
+++ Ecliptix.Core/Ecliptix.Core/Features/Feed/Models/Suggestion.cs
@@ -7,7 +7,7 @@ public sealed record Suggestion
     public required string Id { get; init; }
     public required PostAuthor Author { get; init; }
     public required string Text { get; set; }
-    public string? Title { get; init; } 
+    public string? Title { get; init; }
     public int MutualConnectionsCount { get; init; }
     public bool IsFollowing { get; init; }
     public required DateTime CreatedAt { get; init; }
